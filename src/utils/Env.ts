13df455dd7bb40fb72823fd574ec66603698ca62
Aaron Klinker <aaronklinker1@gmail.com>
2019-01-12 18:27:48 -0600

    Updated env for production

--- src/utils/Env.ts
+++ src/utils/Env.ts
@@ -1,3 +1,18 @@
-export default class Env {
-  public static API_URL = 'http://localhost:8000/api';
+import { NodeEnv } from './types';
+
+const env: NodeEnv = process.env.NODE_ENV as NodeEnv || 'production';
+
+interface ENV {
+  API_URL: string;
 }
+
+const envs: { development: ENV; production: ENV } = {
+  development: {
+    API_URL: 'http://localhost:8000/api',
+  },
+  production: {
+    API_URL: 'https://anime-skip.com/api',
+  },
+};
+
+export default envs[env];
