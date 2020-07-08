d4d6f35f1101e4b96f3d0eadef0592d4680da57e
Aaron Klinker <aaronklinker1@gmail.com>
2020-07-08 18:07:32 -0500

    Added coming soon page, removed docker, setup heroku

---
+++ src/pages/ComingSoon.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="ComingSoon">
+    <img class="banner" alt="Anime Skip Logo" src="../assets/banner.svg" />
+    <p class="message">Coming soon!</p>
+  </div>
+</template>
+
+<script>
+import Vue from 'vue';
+import HelloWorld from '@/components/HelloWorld.vue';
+
+export default Vue.extend({
+  name: 'ComingSoon'
+});
+</script>
+
+<style lang="scss" scoped>
+.ComingSoon {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  height: 100%;
+
+  .banner {
+    width: 50%;
+    max-width: 800px;
+    min-width: 360px;
+  }
+
+  .message {
+    color: white;
+    font-size: 20px;
+    opacity: 0.24;
+    position: absolute;
+    bottom: 16px;
+  }
+}
+</style>
---
+++ src/router.ts
@@ -0,0 +1,28 @@
+import Vue from 'vue';
+import VueRouter, { RouteConfig } from 'vue-router';
+
+Vue.use(VueRouter);
+
+const routes: Array<RouteConfig> = [
+  {
+    path: '/',
+    name: 'ComingSoon',
+    component: () => import(/* webpackChunkName: "coming_soon" */ './pages/ComingSoon.vue')
+  }
+  // {
+  //   path: '/',
+  //   name: 'Home',
+  //   component: () => import(/* webpackChunkName: "home" */ './pages/Home.vue')
+  // }
+  // {
+  //   path: '/about',
+  //   name: 'About',
+  //   component: () => import(/* webpackChunkName: "about" */ './pages/About.vue')
+  // }
+];
+
+export default new VueRouter({
+  mode: 'history',
+  base: process.env.BASE_URL,
+  routes
+});
