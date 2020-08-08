9e42d52aaa9bcdf59bc16f327a54197b0b05aabb
Aaron Klinker <aaronklinker1@gmail.com>
2020-08-08 15:45:34 -0500

    Add alpha tester message

--- src/pages/ComingSoon.vue
+++ src/pages/ComingSoon.vue
@@ -1,7 +1,14 @@
 <template>
-  <Banner class="ComingSoon">
-    <p class="message">Coming soon!</p>
-  </Banner>
+  <div class="ComingSoon">
+    <Banner class="Banner">
+      <p class="message">Coming soon!</p>
+    </Banner>
+    <p class="alpha-testers">
+      Alpha testers, please
+      <router-link to="/sign-up">sign up</router-link>
+      to get started
+    </p>
+  </div>
 </template>
 
 <script>
@@ -17,11 +24,23 @@ export default defineComponent({
 <style lang="scss" scoped>
 .ComingSoon {
   height: 100%;
+  display: flex;
+  flex-direction: column;
+
+  .Banner {
+    flex-grow: 1;
+  }
 
   .message {
     color: rgba($color: #ffffff, $alpha: 0.36);
     font-size: 20px;
     margin-top: 32px;
+  }
+
+  .alpha-testers {
+    align-self: center;
+    margin-bottom: 16px;
+    color: rgba($color: #ffffff, $alpha: 0.36);
   }
 }
 </style>
