54335e2d8d13e501f65334801da71131baf8ee4c
Aaron Klinker <aaronklinker1@gmail.com>
2020-08-09 21:27:13 -0500

    Setup builds to deploy static

--- vue.config.js
+++ vue.config.js
@@ -1,10 +1,15 @@
 const CopyPlugin = require('copy-webpack-plugin');
+const path = require('path');
 
 module.exports = {
+  outputDir: path.resolve(__dirname, 'dist', 'web'),
   configureWebpack: {
     plugins: [
       new CopyPlugin({
-        patterns: [{ from: 'static.json' }],
+        patterns: [
+          { from: 'static.json', to: '../static.json' },
+          { from: 'package.json', to: '../package.json' },
+        ],
       }),
     ],
   },
