34d4126511c528c072a7244f7b8c064721e91f36
Aaron Klinker <aaronklinker1@gmail.com>
2020-08-29 15:43:17 -0500

    Updated alpha urls to 1.0.5

--- src/pages/GetStarted/index.vue
+++ src/pages/GetStarted/index.vue
@@ -298,9 +298,9 @@ export default defineComponent({
 
       currentCard,
       chromeUrl:
-        'https://drive.google.com/file/d/1LLn2F6mCpQcbbyBYoFNE3FvbvE-P3Ns_/view?usp=sharing',
+        'https://drive.google.com/file/d/1jCujH8VPVS9UqrJVwP3soPfgUHrIZ1ex/view?usp=sharing',
       firefoxUrl:
-        'https://drive.google.com/file/d/1RGtsxVb6QLczNV_YuZGGvq4p_Yk-hbs6/view?usp=sharing',
+        'https://drive.google.com/file/d/1txh2LU4AskqXJfK82xMQYZakTc64VfiX/view?usp=sharing',
     };
   },
 });
