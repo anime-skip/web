223e1bf84b83148f6ca5d5332b547bfcce9d4c3c
Aaron Klinker <aaronklinker1@gmail.com>
2020-10-20 22:12:48 -0500

    Setup the recently added list

---
+++ src/components/RecentEpisodesList.vue
@@ -0,0 +1,156 @@
+<template>
+  <div class="RecentEpisodesList">
+    <ul v-if="requestState === RequestState.SUCCESS">
+      <li v-for="episode of recentEpisodes" :key="episode.id">
+        <div class="left">
+          <p class="title">
+            {{ listItemEpisode(episode) }}
+            <span class="episode"></span>
+          </p>
+          <p class="subtitle">
+            <span class="show">{{ listItemShow(episode.show) }}</span>
+            <span>{{ listItemSubtitle(episode) }}</span>
+          </p>
+        </div>
+        <div class="right">
+          <p class="time">{{ listItemTimeSinceCreated(episode) }} ago</p>
+        </div>
+      </li>
+    </ul>
+    <div v-else-if="requestState === RequestState.LOADING">
+      Loading
+    </div>
+    <div v-else>Something went wrong</div>
+  </div>
+</template>
+
+<script lang="ts">
+import api from '@/api';
+import { RequestState } from '@/utils/enums';
+import { Api } from '@anime-skip/types';
+import { defineComponent, ref } from 'vue';
+import EpisodeUtils from '@/utils/EpisodeUtils';
+import TimeUtils from '@/utils/time';
+
+export default defineComponent({
+  setup() {
+    const requestState = ref(RequestState.LOADING);
+    const recentEpisodes = ref<(Api.Episode & { createdAt: number })[]>([]);
+    const fetchEpisodes = async (): Promise<void> => {
+      try {
+        requestState.value = RequestState.LOADING;
+        recentEpisodes.value = await api.recentlyAddedEpisodes(10);
+        requestState.value = RequestState.SUCCESS;
+      } catch (err) {
+        requestState.value = RequestState.FAILURE;
+      }
+    };
+
+    return {
+      fetchEpisodes,
+      recentEpisodes,
+      requestState,
+      RequestState,
+    };
+  },
+  mounted(): void {
+    this.fetchEpisodes();
+  },
+  methods: {
+    listItemShow(show: Api.Show | undefined): string {
+      return show?.name ?? 'Unknown Show';
+    },
+    listItemEpisode(episode: Api.Episode): string {
+      return episode.name ?? 'Unknown Epiosde';
+    },
+    listItemSubtitle(episode: Api.Episode): string {
+      const result = EpisodeUtils.seasonAndNumberDisplay(episode);
+      return result ? ` • ${result}` : '';
+    },
+    listItemTimeSinceCreated(episode: Api.Episode & { createdAt: number }): string {
+      const now = Date.now();
+      /* eslint-disable-next-line @typescript-eslint/no-explicit-any */
+      const createdAt = new Date((episode as any).createdAt);
+      const msDiff = Math.abs(now - createdAt.getTime());
+      console.log({ msDiff, now, createdAt: createdAt.getTime() });
+
+      const weeks = Math.floor(msDiff / TimeUtils.WEEKS);
+      if (weeks >= 1) {
+        return `${weeks} week${weeks === 1 ? '' : 's'}`;
+      }
+      const days = Math.floor(msDiff / TimeUtils.DAYS);
+      if (days >= 1) {
+        return `${days} day${days === 1 ? '' : 's'}`;
+      }
+      const hours = Math.floor(msDiff / TimeUtils.HOURS);
+      if (hours >= 1) {
+        return `${hours} hour${hours === 1 ? '' : 's'}`;
+      }
+      const minutes = Math.floor(msDiff / TimeUtils.MINUTES);
+      if (minutes > 1) {
+        return `${minutes} minutes`;
+      }
+      return '1 minute';
+    },
+  },
+});
+</script>
+
+<style scoped lang="scss">
+.RecentEpisodesList {
+  width: 100%;
+  max-width: 600px;
+  min-height: 300px;
+
+  @media only screen and (min-width: 600px) {
+    background-color: $backgroundColor;
+    border-radius: 16px;
+    box-shadow: 0px 8px 24px 8px rgba(0, 0, 0, 0.24);
+  }
+
+  ul {
+    height: 100%;
+
+    li {
+      display: flex;
+      flex-direction: row;
+      align-items: center;
+      list-style: none;
+      padding: 16px 24px;
+      border-bottom: 1px solid $dark400;
+      &:last-child {
+        border-bottom: none;
+      }
+
+      .left {
+        flex: 1;
+        display: flex;
+        flex-direction: column;
+      }
+
+      .right {
+        flex-shrink: 0;
+        margin-left: 16px;
+      }
+
+      .title {
+        color: $textTitleColor;
+        font-size: 17px;
+      }
+
+      .subtitle,
+      .subtitle * {
+        color: $textSecondaryColor;
+        font-size: 14px;
+        .show {
+          color: $primary500;
+        }
+      }
+
+      .time {
+        color: $textDisabledColor;
+      }
+    }
+  }
+}
+</style>
--- src/utils/time.ts
+++ src/utils/time.ts
@@ -3,4 +3,5 @@ export default class TimeUtils {
   public static MINUTES = 60 * TimeUtils.SECONDS;
   public static HOURS = 60 * TimeUtils.MINUTES;
   public static DAYS = 24 * TimeUtils.HOURS;
+  public static WEEKS = 7 * TimeUtils.DAYS;
 }
