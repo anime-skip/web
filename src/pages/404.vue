45c5d0e9fc7325de303a3963222b2bfd5ada00f5
Aaron Klinker <aaronklinker1@gmail.com>
2021-04-18 15:13:57 -0500

    chore: Refactored the 404 page to use tailwind and new UI library

--- src/pages/404.vue
+++ src/pages/404.vue
@@ -1,35 +1,14 @@
 <template>
   <NavAndFooterLayout>
-    <div class="NotFound">
-      <div class="centered">
-        <h1>404</h1>
-        <p>The page you're looking for doesn't exist</p>
-      </div>
+    <div class="flex-1 flex flex-col items-center justify-center min-h-2/3vh">
+      <p class="heading-1 text-error">404</p>
+      <p class="body-1 text-on-surface">The page you're looking for doesn't exist</p>
     </div>
   </NavAndFooterLayout>
 </template>
 
-<style lang="scss" scoped>
-.NotFound {
-  flex: 1;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  padding: 48px 0;
-
-  .centered {
-    text-align: center;
-
-    h1 {
-      font-size: 192px;
-      font-weight: 500;
-      // color: $textSecondaryColor;
-    }
-
-    p {
-      font-size: 17px;
-      // color: $textSecondaryColor;
-    }
-  }
+<style scoped>
+.min-h-2\/3vh {
+  min-height: 66vh;
 }
 </style>
