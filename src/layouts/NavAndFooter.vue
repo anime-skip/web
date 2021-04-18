9ca321fd154771b72fddf63bba8cc3b243450324
Aaron Klinker <aaronklinker1@gmail.com>
2021-04-18 15:37:25 -0500

    chore: Restore nav bar behavior to match what is was before the UI lib

--- src/layouts/NavAndFooter.vue
+++ src/layouts/NavAndFooter.vue
@@ -1,7 +1,13 @@
 <template>
   <div class="flex flex-col min-h-screen">
-    <nav-bar class="fixed left-0 top-0 right-0 z-10" :items="items" />
-    <div class="flex-grow flex-shrink-0">
+    <nav-bar
+      class="fixed left-0 top-0 right-0 z-10"
+      :items="rightItems"
+      home-title="Anime Skip"
+      :home-icon="homeIcon"
+      home-link="/"
+    />
+    <div class="flex flex-col flex-grow flex-shrink-0">
       <slot />
     </div>
     <SiteFooter class="footer" />
@@ -12,14 +18,32 @@
 import { computed, defineComponent } from 'vue';
 import SiteFooter from '@/components/SiteFooter.vue';
 import { NavBar } from '@anime-skip/ui';
+import homeIcon from '@/assets/logo_nav.svg';
+import { useStore } from 'vuex';
+
+const getStartedLink = { title: 'Get Started', link: '/get-started' };
 
 export default defineComponent({
   components: { NavBar, SiteFooter },
   setup() {
-    const items = computed(() => []);
+    const store = useStore();
+    const isLoggedIn = computed(() => store.getters.IS_SIGNED_IN);
+
+    const leftItems = computed(() => []);
+
+    const rightItems = computed(() => {
+      if (!isLoggedIn.value) return [getStartedLink, { title: 'Log In', link: '/log-in' }];
+
+      return [
+        getStartedLink,
+        { title: 'Account', link: '/account' },
+        { title: 'Log out', link: '/log-out' },
+      ];
+    });
 
     return {
-      items,
+      rightItems,
+      homeIcon,
     };
   },
 });
