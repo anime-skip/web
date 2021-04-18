5099401143d34237931ee446f59ac9a711e90f26
Aaron Klinker <aaronklinker1@gmail.com>
2021-04-18 17:33:01 -0500

    chore: Cleanup forgot password temp UI

--- src/pages/LogIn/ForgotPasswordForm.vue
+++ src/pages/LogIn/ForgotPasswordForm.vue
@@ -1,21 +1,22 @@
 <template>
-  <div class="flex flex-col w-full space-y-3 items-start">
+  <div class="flex flex-col w-full space-y-4 items-start">
     <h4>Forgot Password</h4>
-    <p class="body-1 text-error">Recovering a forgotten password is not available yet</p>
-    <p class="body-1 text-error">Check out the support page to get help</p>
-    <flat-button>
+    <p class="body-2 text-error pb-2">
+      Recovering a forgotten password is not available yet. Check out the support page to get help
+    </p>
+    <raised-button>
       <router-link to="/support">Support</router-link>
-    </flat-button>
+    </raised-button>
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
-import { FlatButton } from '@anime-skip/ui';
+import { RaisedButton } from '@anime-skip/ui';
 
 export default defineComponent({
   components: {
-    FlatButton,
+    RaisedButton,
   },
 });
 </script>
