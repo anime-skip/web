dc17b774717e7f49d7a7c81b4b79e03d1d00e12e
Aaron Klinker <aklinker1@users.noreply.github.com>
2022-05-07 00:29:04 -0500

    chore: correct firebase CSP (#40)

--- src/utils/firebase.ts
+++ src/utils/firebase.ts
@@ -16,9 +16,6 @@ const app = initializeApp(config);
 
 export const remoteConfig = getRemoteConfig(app);
 if (import.meta.env.DEV) remoteConfig.settings.minimumFetchIntervalMillis = 30e3;
-remoteConfig.defaultConfig = {
-  services: '[]',
-};
 fetchAndActivate(remoteConfig);
 
 export interface Service {
@@ -27,3 +24,34 @@ export interface Service {
   background: string;
   supported: boolean;
 }
+
+const defaultServices: Service[] = [
+  {
+    name: 'Cruncyroll',
+    textColor: '#FFFFFF',
+    background: '#F37521',
+    supported: true,
+  },
+  {
+    name: 'Funimation',
+    textColor: '#FFFFFF',
+    background: '#631DB2',
+    supported: true,
+  },
+  {
+    name: 'VRV',
+    textColor: '#000000',
+    background: '#FFDD00',
+    supported: true,
+  },
+  {
+    name: 'Zoro.to',
+    textColor: '#CAE962',
+    background: '#111111',
+    supported: true,
+  },
+];
+
+remoteConfig.defaultConfig = {
+  services: JSON.stringify(defaultServices),
+};
