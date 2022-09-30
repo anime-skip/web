09bd1eace18b41356b03dd34ac1ab193784c3d98
Aaron Klinker <aklinker1@users.noreply.github.com>
2022-09-30 13:06:08 -0500

    make the cards a minimum width
    This makes them appear centered and not offset to a side

--- .v2/src/components/HomeSectionSupportedServicesListItem.vue
+++ .v2/src/components/HomeSectionSupportedServicesListItem.vue
@@ -8,7 +8,7 @@ defineProps<{
 
 <template>
   <li
-    class="min-h-16 flex items-center justify-center text-center font-stylized text-3xl px-6 pt-4 pb-3 rounded"
+    class="min-w-[12rem] min-h-16 flex items-center justify-center text-center font-stylized text-3xl px-6 pt-4 pb-3 rounded"
     :style="`background-color: ${service.background}; color: ${service.textColor}`"
   >
     {{ service.name }}
