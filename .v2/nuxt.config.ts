82fe2feb37c39f48610bfd001d6e1c53598c411e
Aaron Klinker <aklinker1@users.noreply.github.com>
2022-10-09 22:23:43 -0500

    SEO and head changes

--- .v2/nuxt.config.ts
+++ .v2/nuxt.config.ts
@@ -9,6 +9,13 @@ export default defineNuxtConfig({
   },
   app: {
     head: {
+      link: [
+        { rel: 'icon', href: '/favicon.ico' },
+        { rel: 'apple-touch-icon', sizes: '180x180', href: '/static/apple-touch-icon.png' },
+        { rel: 'icon', type: 'image/png', sizes: '32x32', href: '/static/favicon-32x32.png' },
+        { rel: 'icon', type: 'image/png', sizes: '16x16', href: '/static/favicon-16x16.png' },
+        { rel: 'manifest', href: '/site.webmanifest' },
+      ],
       script: [
         {
           async: '',
