a4df1375592027ecfa3d606628adddbec3ed5997
Aaron <aaronklinker1@gmail.com>
2025-01-24 02:02:54 -0600

    chore: Fix build and dev scripts

--- app/vite.config.mts
+++ app/vite.config.mts
@@ -2,9 +2,11 @@ import { defineConfig } from "vite";
 import { join } from "node:path";
 
 export default defineConfig({
+  publicDir: join(__dirname, "../public"),
   build: {
     outDir: join(__dirname, "../.output/public"),
     emptyOutDir: false,
+    copyPublicDir: false,
     rollupOptions: {
       input: [
         join(__dirname, "index.html"),
--- scripts/dev.ts
+++ scripts/dev.ts
@@ -1,6 +1,35 @@
 import { createServer } from "server/server.ts";
+import * as vite from "vite";
 import { generateGraphqlCode } from "./graphql-codegen.ts";
+import { Color, logger } from "server/utils/logger.ts";
 
 await generateGraphqlCode();
+
 const server = await createServer();
+
+const viteServer = await vite.createServer({
+  root: "app",
+  server: {
+    port: 5173,
+    proxy: {
+      "/api": {
+        target: server.state.origin,
+        changeOrigin: true,
+      },
+      "/graphql": {
+        target: server.state.origin,
+        changeOrigin: true,
+      },
+      "/playground": {
+        target: server.state.origin,
+        changeOrigin: true,
+      },
+    },
+  },
+});
+
+await viteServer.listen();
+logger.info(
+  `Vite started   @ ${Color.Underline}${Color.Dim}http://localhost:5173${Color.Reset}`,
+);
 await server.listen({ port: server.state.port });
