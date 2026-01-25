e3caad247ab83bb9e1f0980568288c41d580a5ea
Aaron <aaronklinker1@gmail.com>
2026-01-24 22:55:47 -0600

    feat: Implement third party timestamp resolvers

--- server/graphql/resolvers/third-party-timestamp-resolvers.ts
+++ server/graphql/resolvers/third-party-timestamp-resolvers.ts
@@ -1,8 +1,8 @@
 import type { GqlResolvers } from "server/graphql/resolver-types.gen";
-import { todo } from "shared/utils";
 
 export const thirdPartyTimestampResolvers: GqlResolvers = {
   ThirdPartyTimestamp: {
-    type: (_parent, _args, _ctx) => todo(),
+    type: (parent, _args, ctx) =>
+      ctx.dataloaders.timestampTypes.load(parent.typeId),
   },
 };
